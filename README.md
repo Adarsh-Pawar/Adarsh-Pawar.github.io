9adcedf618b4f507cc629ac4759e51f4005f77e2
Adarsh Pawar <pawar.adarsh8178@gmail.com>
2024-08-11 12:48:56 +0530

    Transferred portfolio from my_portfolio repo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-<h1><a href="/My_Portfolio/">My portfolio-</a></h1>
+<h1>My Portfolio</h1>
+<h3>You can visit my portfolio by visiting here:- <a href="https://pawaradarsh.me/My_Portfolio/">pawaradarsh.me/My_Portfolio</a></h3>
---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const date = document.getElementById("date");
+date.innerHTML=new Date().getFullYear();
